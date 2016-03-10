e102a030a7db535bb8c89a81f69c6d897fa73901
Andriy Yarish <andriy_yarish@epam.com>
2016-03-10 12:35:58 +0200

    few methods added

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,5 @@
 import Entities.Event;
+import Services.EventService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -7,9 +8,18 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class App {
 static Event event;
+static EventService service;
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("Spring.xml");
         event = (Event) context.getBean("event");
+        service = (EventService) context.getBean("eventService");
+        service.addEvent(event);
+        Event event2 = event;
+        event2.setName("event 2");
+        service.addEvent(event2);
+        service.removeEventByIndex(3);
+
         System.out.println(event.toString());
+        System.out.println(service.getEventMap().toString());
     }
 }
--- src/main/java/Services/EventService.java
+++ src/main/java/Services/EventService.java
@@ -9,11 +9,24 @@ import java.util.Map;
  * Created by Andriy_Yarish on 3/9/2016.
  */
 public class EventService {
-    private Map<Integer, Event> eventList = new LinkedHashMap<Integer, Event>();
-    private int index = 0;
+    private static Map<Integer, Event> eventList = new LinkedHashMap<Integer, Event>();
+    private static int index = 0;
 
     public void addEvent(Event event){
         eventList.put(index,event);
         index ++;
+    }
+
+    public Map<Integer,Event> getEventMap (){
+        return eventList;
+    }
+
+    public void removeEventByIndex(int key) {
+
+        if(eventList.remove(index)==null)
+            System.out.println("!!! Caution - remove operation failed. There are no value mapped to this key");
+
+
+
     }
 }
